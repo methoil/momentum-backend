645555b6e3755ee58dededbc2bcc9b6064ea8b14
methoil <mateev.br@gmail.com>
2020-10-27 22:44:22 -0400

    feat - user endpoints to logout

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -33,4 +33,24 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(token => token !== req.token);
+        await req.user.save();
+        res.send();
+    } catch (err) {
+        res.status(500).send('Error logging out');
+    }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send();
+    } catch (err) {
+        res.status(500).send('Error logging out');
+    }
+});
+
 module.exports = router;
